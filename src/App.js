0abcec710c3beb37453206aefe6b88e927de0d52
mdalamin123a <md.alamindev90@gmail.com>
2024-10-21 23:31:47 +0600

    32-7-pass-dynamic-data-to-component-props

--- src/App.js
+++ src/App.js
@@ -2,42 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var person = {
-    name:'Dr. Mhafuz',
-    job:'Singer',
-  };
-  var person2 = {
-    name:'Eva Rahman',
-    job:'Kokil Konthi',
-  };
-  var style = {
-    color: "red",
-    backgroundColor: 'yellow',
-  }
+  const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman"];
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
-        <Person></Person>
-        <Person></Person>
-        <Person></Person>
+        <Person name="Munna" profession="Engeneer"></Person>
+        <Person name="Masud" profession="Football"></Person>
         <Person></Person>
       </header>
     </div>
   );
 }
 
-function Person(){
-  const personStyle = {
-    border: "2px solid red",
-    margin: "10px",
-  }
-  return(
-    <div style={{border: "2px solid yellow", margin: "10px",}}>
-      <h1>Name: Shakib</h1>
-      <h3>Hero of the year</h3>
-   </div>
+function Person(test){
+  return (
+    <div style={{border:'2px solid gold', margin:"10px", width:"400px"}}>
+      <h3>My Name: {test.name}</h3>
+      <p>My Profession: {test.profession}</p>
+    </div>
   );
-} 
+}
 
 export default App;
