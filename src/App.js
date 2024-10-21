723f539a86e40c2ccafb5b8f750fcd11fc70bd42
mdalamin123a <md.alamindev90@gmail.com>
2024-10-21 11:16:28 +0600

    32-6-create-component

--- src/App.js
+++ src/App.js
@@ -17,26 +17,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        {/* <h1>My Heading: {(2+5)*25+65}</h1> */}
-        <h1 className='' style={style}>My Heading: {person.name + ' ' + person.job}</h1>
-        <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Singer: {person2.name + ' ' + person2.job}</h3>
-        <p>My First React Paragraph</p>
+        <p>I am a react person</p>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
 
+function Person(){
+  const personStyle = {
+    border: "2px solid red",
+    margin: "10px",
+  }
+  return(
+    <div style={{border: "2px solid yellow", margin: "10px",}}>
+      <h1>Name: Shakib</h1>
+      <h3>Hero of the year</h3>
+   </div>
+  );
+} 
+
 export default App;
-
-
-/**
- * We can write html on js return(). this is called as jsx. 
- * {} used to print js expression or dynamic content under jsx.
- * className='' is used to give a class name on jsx tag/element
- * style={{backgroundColor:'red', color:'yellow'}} is used to give incline css/style on tag/element 
- * import "./App.css" is used to load external css file
- */
