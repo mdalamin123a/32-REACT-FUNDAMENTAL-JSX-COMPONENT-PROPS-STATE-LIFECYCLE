13fec5c44815fedac822f3534418c0b749ae580d
mdalamin123a <md.alamindev90@gmail.com>
2024-10-21 10:07:11 +0600

    32-4-jsx-dynamic-content-dynamic-style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person = {
+    name:'Dr. Mhafuz',
+    job:'Singer',
+  };
+  var person2 = {
+    name:'Eva Rahman',
+    job:'Kokil Konthi',
+  };
+  var style = {
+    color: "red",
+    backgroundColor: 'yellow',
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +21,22 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
+        {/* <h1>My Heading: {(2+5)*25+65}</h1> */}
+        <h1 className='' style={style}>My Heading: {person.name + ' ' + person.job}</h1>
+        <h3 style={{backgroundColor:'cyan', color:'yellow'}}>Singer: {person2.name + ' ' + person2.job}</h3>
+        <p>My First React Paragraph</p>
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+/**
+ * We can write html on js return(). this is called as jsx. 
+ * {} used to print js expression or dynamic content under jsx.
+ * className='' is used to give a class name on jsx tag/element
+ * style={{backgroundColor:'red', color:'yellow'}} is used to give incline css/style on tag/element 
+ * import "./App.css" is used to load external css file
+ */
