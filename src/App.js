e3447a3d807ec2dbd15a5d62ed82eac6dd60774f
mdalamin123a <md.alamindev90@gmail.com>
2024-10-22 00:08:08 +0600

    32-8-pass-object-to-component-and-access-object

--- src/App.js
+++ src/App.js
@@ -3,14 +3,43 @@ import './App.css';
 
 function App() {
   const nayoks = ["Anwar", "Jafor", "Alomgir", "Salman"];
+  const products = [
+    {name: 'PhotoShop', price: '$90.99'},
+    {name: 'Illustrator', price: '$60.99'},
+    {name: 'PDF Reader', price: '$6.99'},
+  ];
   return (
     <div className="App">
       <header className="App-header">
         <p>I am a react person</p>
+        {/* <Product name={products[0].name} price={products[0].price}></Product> */}
+        {/* Pass Object to Component */}
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
         <Person name="Munna" profession="Engeneer"></Person>
         <Person name="Masud" profession="Football"></Person>
         <Person></Person>
       </header>
+    </div>
+  );
+}
+
+function Product(props){
+  const productStyles = {
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left',
+  };
+  const {name, price} = props.product;
+  console.log(props);
+  return(
+    <div style={productStyles}>
+      <h2>{name}</h2>
+      <h5>{price}</h5>
+      <button>Buy Now</button>
     </div>
   );
 }
@@ -25,3 +54,7 @@ function Person(test){
 }
 
 export default App;
+
+/**
+ * Pass obj to component
+ */
